9038f0e167865b8b3f6844649569937ecb031822
ghazaleh rastegar <ghazaleh.rastegar74@gmail.com>
2021-12-03 16:12:43 +0330

    desktop footer ui has been implemented

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,30 @@
       <span class="login-btn font-bold">Login</span>
       <span class="sign-up-btn font-bold">Sign Up</span>
     </div>
+
+    <router-view></router-view>
+    <footer class="footer">
+      <img src="./assets/images/white-logo.svg" class="logo" />
+      <div v-for="(item, i) in footerItems" :key="'cat' + i">
+        <span class="category">{{ item.category }}</span>
+        <div class="sub-categories">
+          <span
+            v-for="(item, i) in item.subcategory"
+            :key="'sub' + i"
+            class="sub-category"
+            >{{ item }}</span
+          >
+        </div>
+      </div>
+      <div class="icons">
+        <img
+          v-for="(item, index) in icons"
+          :key="'icon' + index"
+          :src="item.icon"
+          :class="index !== icons.length - 1 ? 'icon' : ''"
+        />
+      </div>
+    </footer>
   </div>
 </template>
 
@@ -42,6 +66,25 @@ export default class App extends Vue {
       link: "",
     },
   ];
+
+  private footerItems = [
+    {
+      category: "Features",
+      subcategory: ["Link Shortening", "Branded Links", "Analytics"],
+    },
+    { category: "Resources", subcategory: ["Blog", "Developers", "Support"] },
+    {
+      category: "Company",
+      subcategory: ["About", "Our Team", "Careers", "Contact"],
+    },
+  ];
+
+  private icons = [
+    { icon: require("./assets/images/icon-facebook.svg") },
+    { icon: require("./assets/images/icon-twitter.svg") },
+    { icon: require("./assets/images/icon-pinterest.svg") },
+    { icon: require("./assets/images/icon-instagram.svg") },
+  ];
 }
 </script>
 
@@ -49,7 +92,8 @@ export default class App extends Vue {
 #app {
   position: relative;
   background: white;
-  height: 100%;
+  height: 100vh;
+  overflow-y: auto;
   .navbar {
     position: sticky;
     top: 0;
@@ -90,6 +134,42 @@ export default class App extends Vue {
       color: white;
       font-size: 12px;
       cursor: pointer;
+    }
+  }
+  .footer {
+    padding: 64px;
+    background-color: var(--neutral3);
+    color: white;
+    font-size: 12px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: start;
+    .logo {
+      margin-right: 64px;
+    }
+    .category {
+      font-weight: 800;
+    }
+    .sub-categories {
+      display: flex;
+      flex-direction: column;
+      justify-content: start;
+      align-items: start;
+      margin-top: 24px;
+
+      .sub-category {
+        margin-bottom: 16px;
+      }
+    }
+    .icons {
+      display: flex;
+      flex-direction: row;
+      justify-content: start;
+      align-items: start;
+      .icon {
+        margin-right: 16px;
+      }
     }
   }
 }
